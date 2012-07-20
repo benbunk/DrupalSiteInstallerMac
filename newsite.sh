5487d6c3ae064c200e3eed09b2fd6b582a40ab43
Ben Bunk <benbunk@gmail.com>
2012-07-20 10:03:07 -0400

    Modified the script to checkout drupal from d.o

--- newsite.sh
+++ newsite.sh
@@ -3,13 +3,16 @@
 # Requirements:
 #  1. Drush pre installed and part of the path
 #  2. Apache setup with user home directories
+#  3. Git installed and part of the path
 
 # Setup the variables and echo them out for debugging
 SITENAME=$1
 
 echo Creating Site: $SITENAME
-mkdir ~/Sites/$SITENAME
-cp -a ~/Sites/drupal-7.14/ ~/Sites/$SITENAME
+# Uncomment the 2 lines below and comment the third to copy from an existing clean download of drupal.
+#mkdir ~/Sites/$SITENAME
+#cp -a ~/Sites/drupal-7.14/ ~/Sites/$SITENAME
+git clone --recursive --branch 7.x http://git.drupal.org/project/drupal.git $SITENAME
 
 echo Setup the settings.
 cd ~/Sites/$SITENAME/sites/default
