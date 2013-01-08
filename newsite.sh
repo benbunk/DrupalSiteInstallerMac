d0b6d1f175cd24fdad6c0e5203c8048b829d4ede
Ben Bunk <benbunk@gmail.com>
2013-01-08 17:43:47 -0500

    Added profile support and fixed another path relative bug.

--- newsite.sh
+++ newsite.sh
@@ -24,6 +24,14 @@ MYSQLHOST=127.0.0.1
 VERSION=$1
 SITENAME=$2
 REPO=$3
+PROFILE=$4
+
+# Create blank DB if option one is DB and option two is a DB name.
+
+# Default Profile value
+if [ -z "$PROFILE" ]; then
+  PROFILE="standard"
+fi
 
 # Default repo is github -- this is 10x faster then d.o
 REPOURL="git://github.com/drupal/drupal.git"
@@ -37,7 +45,7 @@ if [ "$REPO" = "local" ]; then
 fi
 
 echo Creating Site: $SITENAME from Repo: $REPOURL
-git clone --recursive --branch $VERSION.x $REPOURL $SITENAME
+git clone --recursive --branch $VERSION.x $REPOURL ~/Sites/$SITENAME
 
 # Check if the local repo for this version exists. If not create it for caching.
 if [ ! -e ~/Sites/drupal-$VERSION ]; then 
@@ -61,11 +69,9 @@ fi
 
 echo Drush Site Install.
 if [ "$VERSION" = "7" ]; then
-  drush site-install standard --account-name=admin --account-pass=password --account-mail=benbunk@example.com --db-url=mysql://$MYSQLUSER:$MYSQLPASS@$MYSQLHOST:3306/$SITENAME --site-name=$SITENAME --yes
-  drush dis dblog, update, color, comment, dashboard, help, overlay, search, shortcut --yes
-  drush en syslog --yes
+  drush site-install $PROFILE --account-name=admin --account-pass=password --account-mail=benbunk@example.com --db-url=mysql://$MYSQLUSER:$MYSQLPASS@$MYSQLHOST:3306/$SITENAME --site-name=$SITENAME --yes
 elif [ "$VERSION" = "8" ]; then
-  drush site-install standard --account-name=admin --account-pass=password --account-mail=benbunk@example.com --db-url=mysql://$MYSQLUSER:$MYSQLPASS@$MYSQLHOST:3306/$SITENAME --site-name=$SITENAME --yes
+  drush site-install $PROFILE --account-name=admin --account-pass=password --account-mail=benbunk@example.com --db-url=mysql://$MYSQLUSER:$MYSQLPASS@$MYSQLHOST:3306/$SITENAME --site-name=$SITENAME --yes
 else
   # @todo - This always fails for Drupal 6 on my machine. It does however 
   #         create the DB so all that's needed is to finish the install in the 
